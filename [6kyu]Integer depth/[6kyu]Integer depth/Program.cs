e67a64ffc6b257d840659c8bb6a166dc97b8d829
Ilya Ovsyannikov <ilya.ovsyannikov345@gmail.com>
2023-02-14 22:53:58 +0300

    [6kyu]Integer depth.

---
+++ [6kyu]Integer depth/[6kyu]Integer depth/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Solution
+{
+    public class Kata
+    {
+        public static int ComputeDepth(int number)
+        {
+            List<int> digits = GetListOfDigits(number);
+
+            List<int> reachedDigits = new List<int>(digits);
+
+            int iterationsCount = 1;
+
+            while (reachedDigits.Count != 10)
+            {
+                iterationsCount++;
+
+                digits = GetListOfDigits(number * iterationsCount);
+
+                reachedDigits = reachedDigits.Union(digits)
+                                             .ToList();
+            }
+
+            return iterationsCount;
+        }
+
+        private static List<int> GetListOfDigits(int number)
+        {
+            return number.ToString()
+                         .Select(x => int.Parse(x.ToString()))
+                         .ToList();
+        }
+    }
+}

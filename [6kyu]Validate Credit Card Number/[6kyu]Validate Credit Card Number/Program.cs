35b3a3ffdee3958e6c65396b03c0b96f28553ff0
Ilya Ovsyannikov <ilya.ovsyannikov345@gmail.com>
2023-02-14 19:29:23 +0300

    Added [6kyu]Validate Credit Card Number.

---
+++ [6kyu]Validate Credit Card Number/[6kyu]Validate Credit Card Number/Program.cs
@@ -0,0 +1,22 @@
+﻿solution solution = new solution();
+
+solution.validate("477 073 360");
+
+public class solution
+{
+    public bool validate(string cardNumber)
+    {
+        int[] digits = cardNumber.Where(x => char.IsDigit(x))
+                                 .Select(x => int.Parse(x.ToString()))
+                                 .ToArray();
+
+        for (var i = digits.Length - 2; i >= 0; i-=2)
+        {
+            int doubledDigit = digits[i] * 2;
+
+            digits[i] = doubledDigit < 10 ? doubledDigit : doubledDigit - 9;
+        }
+
+        return digits.Sum() % 10 == 0;
+    }
+}

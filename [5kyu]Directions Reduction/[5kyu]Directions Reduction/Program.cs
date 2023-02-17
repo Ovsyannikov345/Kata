c70a1bb522e250a10385e4791dfa8b9858f43051
Ilya Ovsyannikov <ilya.ovsyannikov345@gmail.com>
2023-02-17 23:02:59 +0300

    [5kyu]Directions Reduction.

---
+++ [5kyu]Directions Reduction/[5kyu]Directions Reduction/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class DirReduction
+{
+    private static readonly (string, string)[] oppositeDirections =
+    {
+        ("NORTH", "SOUTH"),
+        ("SOUTH", "NORTH"),
+        ("EAST", "WEST"),
+        ("WEST", "EAST"),
+    };
+
+    public static string[] dirReduc(string[] directions)
+    {
+        List<string> reducedDirections = new List<string>(directions);
+
+        for (var i = 0; i < reducedDirections.Count - 1; i++)
+        {
+            if (reducedDirections.Count < 2)
+            {
+                break;
+            }
+
+            if (oppositeDirections.Contains((reducedDirections[i], reducedDirections[i + 1])))
+            {
+                reducedDirections.RemoveRange(i, 2);
+
+                i -= i != 0 ? 2 : 1;
+            }
+        }
+
+        return reducedDirections.ToArray();
+    }
+}

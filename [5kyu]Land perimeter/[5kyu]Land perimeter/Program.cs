f7e41d1712e2a912fc3d73e8c2500bb37fbc1543
Ilya Ovsyannikov <ilya.ovsyannikov345@gmail.com>
2023-02-19 16:02:45 +0300

    Land perimeter

---
+++ [5kyu]Land perimeter/[5kyu]Land perimeter/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+
+public static class LandPerimeter
+{
+    public static readonly int sideLength = 1;
+
+    public static readonly char landMark = 'X';
+
+    public static string Calculate(string[] map)
+    {
+        char[][] field = map.Select(x => x.ToCharArray()).ToArray();
+
+        int totalPerimeter = 0;
+
+        for (var i = 0; i < field.Length; i++)
+        {
+            for (var j = 0; j < field[i].Length; j++)
+            {
+                if (field[i][j] == landMark)
+                {
+                    totalPerimeter += GetTilePerimeter(field, i, j);
+                }
+            }
+        }
+
+        return $"Total land perimeter: {totalPerimeter}";
+    }
+
+    private static int GetTilePerimeter(char[][] field, int i, int j)
+    {
+        int tilePerimeter = 0;
+
+        if (j + 1 >= field[i].Length || field[i][j + 1] != landMark)
+        {
+            tilePerimeter++;
+        }
+
+        if (i + 1 >= field.Length || field[i + 1][j] != landMark)
+        {
+            tilePerimeter++;
+        }
+
+        if (j - 1 < 0 || field[i][j - 1] != landMark)
+        {
+            tilePerimeter++;
+        }
+
+        if (i - 1 < 0 || field[i - 1][j] != landMark)
+        {
+            tilePerimeter++;
+        }
+
+        return tilePerimeter;
+    }
+}

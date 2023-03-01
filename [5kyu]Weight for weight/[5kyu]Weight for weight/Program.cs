8d03494b86962a4e630805dc18791cb8f06e4cfb
Ilya Ovsyannikov <ilya.ovsyannikov345@gmail.com>
2023-03-01 21:22:19 +0300

    Added [5kyu]Weight for weights

---
+++ [5kyu]Weight for weight/[5kyu]Weight for weight/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class WeightSort
+{
+	public static string orderWeight(string source)
+	{
+        string[] weights = source.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+        Array.Sort(weights, new WeightComparer());
+
+        return string.Join(' ', weights);
+	}
+}
+
+public class WeightComparer : IComparer<string>
+{
+    public int Compare(string x, string y)
+    {
+        int weightDifference = GetWeight(x) - GetWeight(y);
+
+        if (weightDifference != 0)
+        {
+            return weightDifference;
+        }
+
+        return x.CompareTo(y);
+    }
+
+    private static int GetWeight(string number)
+    {
+        return number.Select(digit => int.Parse(digit.ToString()))
+                     .Sum();
+    }
+}

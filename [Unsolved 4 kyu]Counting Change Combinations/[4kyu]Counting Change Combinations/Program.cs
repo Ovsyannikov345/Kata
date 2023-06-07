5c3e11499b344d93fb09fdfbe02eecec4cd79449
Ilya Ovsyannikov <ilya.ovsyannikov345@gmail.com>
2023-06-07 16:55:46 +0300

    Resolved a kata

--- [Unsolved 4 kyu]Counting Change Combinations/[4kyu]Counting Change Combinations/Program.cs
+++ [Unsolved 4 kyu]Counting Change Combinations/[4kyu]Counting Change Combinations/Program.cs
@@ -1,87 +1,34 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-
-namespace _4kyu_Counting_Change_Combinations
-{
-    public class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine(Kata.CountCombinations(10000, new[] { 1,2,3,4 }));
-        }
-    }
-}
 
 public static class Kata
 {
     public static int CountCombinations(int money, int[] coins)
     {
-        int maxCoinsCount = money / coins.Min();
+        Array.Sort(coins);
+        Array.Reverse(coins);
 
-        List<List<int>> combinations = GetAllCombinations(money, coins, maxCoinsCount);
-
-        List<List<int>> uniqueCombinations = GetUniqueCombinations(combinations);
-
-        return uniqueCombinations.Count;
+        return CountCombinationsRecursive(money, coins);
     }
 
-    private static List<List<int>> GetAllCombinations(int amount, int[] coins, int remainingIterations)
+    private static int CountCombinationsRecursive(int money, int[] coins)
     {
-        if (remainingIterations == 0)
+        if (money == 0)
         {
-            List<List<int>> result = new List<List<int>>();
-
-            foreach (int coin in coins)
-            {
-                result.Add(new List<int> { coin });
-            }
-
-            return result;
+            return 1;
         }
 
-        List<List<int>> totalCombinations = new List<List<int>>();
-
-        foreach (int coin in coins)
+        if (money < 0 || coins.Length == 0)
         {
-            List<List<int>> currentCombinations = GetAllCombinations(amount, coins, remainingIterations - 1)
-                                                 .ToList();
-
-            foreach (List<int> combination in currentCombinations)
-            {
-                int combinationSum = combination.Sum();
-
-                if (combinationSum == amount)
-                {
-                    totalCombinations.Add(combination);
-                }
-                else if (combinationSum + coin <= amount)
-                {
-                    combination.Add(coin);
-                    totalCombinations.Add(combination);
-                }
-            }
+            return 0;
         }
 
-        return totalCombinations;
-    }
+        int combinationsCount = 0;
 
-    private static List<List<int>> GetUniqueCombinations(List<List<int>> combinations)
-    {
-        List<List<int>> uniqueCombinations = new List<List<int>>();
-
-        foreach (List<int> combination in combinations)
+        for (var i = 0; i < money / coins[0] + 1; i++)
         {
-            combination.Sort();
-
-            if (uniqueCombinations.Exists(x => x.SequenceEqual(combination)))
-            {
-                continue;
-            }
-
-            uniqueCombinations.Add(combination);
+            combinationsCount += CountCombinationsRecursive(money - coins[0] * i, coins[1..]);
         }
 
-        return uniqueCombinations;
+        return combinationsCount;
     }
 }

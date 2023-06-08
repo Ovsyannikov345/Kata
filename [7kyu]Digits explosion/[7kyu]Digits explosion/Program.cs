ca41cc60bd6c13eaa279f56ae62facd3e704d1d6
Ilya Ovsyannikov <ilya.ovsyannikov345@gmail.com>
2023-06-08 23:11:55 +0300

    Added new kata

---
+++ [7kyu]Digits explosion/[7kyu]Digits explosion/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+
+public class Digits
+{
+    public static string Explode(string source)
+    {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        foreach (char digit in source)
+        {
+            stringBuilder.Append(new string(digit, int.Parse(digit.ToString())));
+        }
+
+        return stringBuilder.ToString();
+    }
+}

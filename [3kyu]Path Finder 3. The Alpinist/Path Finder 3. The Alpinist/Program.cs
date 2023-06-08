f072702bad8da386e8b1df35e967233d94b74c41
Ilya Ovsyannikov <ilya.ovsyannikov345@gmail.com>
2023-06-09 02:12:27 +0300

    Solved 1 kata and added 1 unresolved kata

---
+++ [3kyu]Path Finder 3. The Alpinist/Path Finder 3. The Alpinist/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+string d = "0707\n" +
+           "7070\n" +
+           "0707\n" +
+           "7070";
+
+Finder.PathFinder(d);
+
+public class Finder
+{
+    public static int PathFinder(string maze)
+    {
+        string[] lines = maze.Split('\n');
+
+        int[][] map = lines.Select(line => line.ToCharArray()
+                                               .Select(cell => cell - '0')
+                                               .ToArray())
+                           .ToArray();
+
+        int FindEffectiveWay(int[][] map, bool[][] visitedCells, int i, int j)
+        {
+            if (i > 0 && !visitedCells[i - 1][j])
+            {
+
+            }
+
+            if (i < map.Length - 1 && !visitedCells[i + 1][j])
+            {
+
+            }
+        }
+    }
+}
---
+++ [4kyu]Path Finder 4. Where are you/[4kui]Path Finder 4. Where are you/Program.cs
@@ -0,0 +1,64 @@
+﻿using System.Drawing;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+public enum Direction
+{
+    Up = 0,
+    Right = 1,
+    Down = 2,
+    Left = 3,
+}
+
+public class PathFinder
+{
+    private static int currentX = 0;
+
+    private static int currentY = 0;
+
+    private static Direction currentDirection = Direction.Left;
+
+    public static Point iAmHere(string path)
+    {
+        string[] commands = Regex.Matches(path, @"\d+|[rlRL]")
+                                 .Select(x => x.Value)
+                                 .ToArray();
+
+        foreach (var command in commands)
+        {
+            if (int.TryParse(command, out int steps))
+            {
+                switch (currentDirection)
+                {
+                    case Direction.Up:
+                        currentY += steps;
+                        break;
+                    case Direction.Down:
+                        currentY -= steps;
+                        break;
+                    case Direction.Right:
+                        currentX += steps;
+                        break;
+                    case Direction.Left:
+                        currentX -= steps;
+                        break;
+                }
+            }
+            else
+            {
+                int newDirection = (int)currentDirection + command switch
+                {
+                    "r" => 1,
+                    "l" => 3,
+                    _ => 2,
+                };
+
+                newDirection %= 4;
+
+                currentDirection = (Direction)newDirection;
+            }
+        }
+
+        return new Point(currentX, currentY);
+    }
+}

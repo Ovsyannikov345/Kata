63547f59f98ab3a6441251ffcb05f6925a62428d
Ilya Ovsyannikov <ilya.ovsyannikov345@gmail.com>
2023-08-13 21:06:20 +0300

    feat: add new kata

---
+++ [5kyu]Not very secure/[5kyu]Not very secure/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.RegularExpressions;
+
+public class Kata
+{
+    public static bool Alphanumeric(string str)
+    {
+        var validPart = Regex.Matches(str, @"[a-zA-Z0-9]+");
+
+        return validPart.Count == 1 && validPart[0].Value.Length == str.Length;
+    }
+}

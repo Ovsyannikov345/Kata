a502d36505442ebe23696c90af88447ae1b25e68
Ilya Ovsyannikov <ilya.ovsyannikov345@gmail.com>
2023-08-13 21:14:55 +0300

    feat: add new kata

---
+++ [8kyu]Remove First and Last Character Part Two/[8kyu]Remove First and Last Character Part Two/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public static class Kata
+{
+    public static string Array(string sequence)
+    {
+        string[] elements = sequence.Split(',', StringSplitOptions.TrimEntries);
+
+        if (elements.Length < 3)
+        {
+            return null;
+        }
+
+        return string.Join(' ', elements[1..^1]);
+    }
+}

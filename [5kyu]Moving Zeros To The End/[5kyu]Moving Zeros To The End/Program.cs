ef6e967cee4c5025b4986a876eb2678829475d6f
Ilya Ovsyannikov <ilya.ovsyannikov345@gmail.com>
2023-08-16 21:01:50 +0300

    feat: add new kata

---
+++ [5kyu]Moving Zeros To The End/[5kyu]Moving Zeros To The End/Program.cs
@@ -0,0 +1,19 @@
+﻿public class Kata
+{
+    public static int[] MoveZeroes(int[] arr)
+    {
+        int[] result = new int[arr.Length];
+
+        int placementIndex = 0;
+
+        foreach (var item in arr)
+        {
+            if (item != 0)
+            {
+                result[placementIndex++] = item;
+            }
+        }
+
+        return result;
+    }
+}

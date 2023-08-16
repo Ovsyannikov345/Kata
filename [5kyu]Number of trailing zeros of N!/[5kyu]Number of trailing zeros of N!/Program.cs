23d841b3ca155c777d7f3a4d0ad946ced8272c53
Ilya Ovsyannikov <ilya.ovsyannikov345@gmail.com>
2023-08-17 00:23:31 +0300

    feat: add new kata

---
+++ [5kyu]Number of trailing zeros of N!/[5kyu]Number of trailing zeros of N!/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public static class Kata
+{
+    public static int TrailingZeros(int n)
+    {
+        // Just some mathematic formulas.
+        int kMax = (int)Math.Log(n, 5);
+
+        int zerosCount = 0;
+
+        for (var k = 1; k <= kMax; k++)
+        {
+            zerosCount += (int)(n / Math.Pow(5, k));
+        }
+
+        return zerosCount;
+    }
+}

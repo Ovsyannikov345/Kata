d11a458b3ef15de576c4ed0acf7ae14e8f860833
Ilya Ovsyannikov <ilya.ovsyannikov345@gmail.com>
2023-08-16 23:37:44 +0300

    feat: add new kata

---
+++ [4kyu]Strip Comments/[4kyu]Strip Comments/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+public class StripCommentsSolution
+{
+    public static string StripComments(string text, string[] symbols)
+    {
+        string[] lines = text.Split(new char[] { '\n' });
+
+        string commentSymbols = string.Join("", symbols);
+
+        for (var i = 0; i < lines.Length; i++)
+        {
+            lines[i] = Regex.Replace(lines[i], $@"\s*[{commentSymbols}]+.*$", "").TrimEnd();
+        }
+
+        return string.Join("\n", lines);
+    }
+}

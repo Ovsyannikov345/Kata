91e0c5390d9351ca9314007589ce4fb25dace961
Ilya Ovsyannikov <ilya.ovsyannikov345@gmail.com>
2023-08-16 23:02:43 +0300

    feat: add new kata

---
+++ [5kyu]Rot13/[5kyu]Rot13/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+
+public class Kata
+{
+    public static string Rot13(string message)
+    {
+        StringBuilder encodedMessage = new();
+
+        foreach (char symbol in message)
+        {
+            if (!char.IsLetter(symbol))
+            {
+                encodedMessage.Append(symbol);
+                continue;
+            }
+
+            char encodedSymbol = (char)(symbol + 13);
+
+            if (symbol <= 'Z' && encodedSymbol > 'Z')
+            {
+                encodedSymbol = (char)('A' + encodedSymbol - 'Z' - 1);
+            }
+            else if (encodedSymbol > 'z')
+            {
+                encodedSymbol = (char)('a' + encodedSymbol - 'z' - 1);
+            }
+
+            encodedMessage.Append(encodedSymbol);
+        }
+
+        return encodedMessage.ToString();
+    }
+}

9f22814531865237375ee6dff5dc47df9bbf699f
Ilya Ovsyannikov <ilya.ovsyannikov345@gmail.com>
2023-08-16 20:54:03 +0300

    feat: add new kata

---
+++ [6kyu]Equal Sides Of An Array/[6kyu]Equal Sides Of An Array/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+public class Kata
+{
+    public static int FindEvenIndex(int[] arr)
+    {
+        int rightSum = arr.Skip(1).Sum();
+
+        if (rightSum == 0)
+        {
+            return 0;
+        }
+
+        int leftSum = 0;
+
+        for (var i = 1; i < arr.Length; i++)
+        {
+            leftSum += arr[i - 1];
+            rightSum -= arr[i];
+
+            if (rightSum == leftSum)
+            {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}

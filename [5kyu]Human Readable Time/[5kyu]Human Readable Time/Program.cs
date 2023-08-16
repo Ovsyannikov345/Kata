af711300a289f0cb07e400a1f625ff4f7838898c
Ilya Ovsyannikov <ilya.ovsyannikov345@gmail.com>
2023-08-16 21:47:55 +0300

    feat: add new kata

---
+++ [5kyu]Human Readable Time/[5kyu]Human Readable Time/Program.cs
@@ -0,0 +1,7 @@
+﻿public static class TimeFormat
+{
+    public static string GetReadableTime(int seconds)
+    {
+        return $"{seconds / 3600:D2}:{seconds % 3600 / 60:D2}:{seconds % 60:D2}";
+    }
+}

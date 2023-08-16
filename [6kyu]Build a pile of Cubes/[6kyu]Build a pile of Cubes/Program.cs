b006bf90e1a09ab75067a77455258dc8e788c344
Ilya Ovsyannikov <ilya.ovsyannikov345@gmail.com>
2023-08-16 22:21:56 +0300

    feat: add new kata

---
+++ [6kyu]Build a pile of Cubes/[6kyu]Build a pile of Cubes/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class ASum
+{
+	public static long findNb(long volume)
+	{
+		int largestSize = 0;
+
+		while (volume > 0)
+        {
+			largestSize++;
+			volume -= (long)Math.Pow(largestSize, 3);
+        }
+
+		if (volume == 0)
+        {
+			return largestSize;
+        }
+
+		return -1;
+	}
+}
---
+++ [6kyu]Count the smiley faces!/[6kyu]Count the smiley faces!/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+using System.Text.RegularExpressions;
+
+public static class Kata
+{
+    public static int CountSmileys(string[] smileys)
+    {
+        return smileys.Count(e => IsSmiley(e));
+    }
+
+    private static bool IsSmiley(string text) => Regex.IsMatch(text, @"^[:;][-~]{0,1}[)D]$");
+}

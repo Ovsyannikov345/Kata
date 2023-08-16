1e337d00de3a03ac27cfa4352c436a44d080dbe7
Ilya Ovsyannikov <ilya.ovsyannikov345@gmail.com>
2023-08-16 21:34:08 +0300

    feat: add new kata

---
+++ [5kyu]Simple Pig Latin/[5kyu]Simple Pig Latin/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+public class Kata
+{
+    public static string PigIt(string str)
+    {
+        MatchCollection matches = Regex.Matches(str, @"\w+");
+
+        StringBuilder stringBuilder = new StringBuilder(str);
+
+        foreach (Match match in matches)
+        {
+            stringBuilder.Replace(match.Value, TranslateWord(match.Value), match.Index + stringBuilder.Length - str.Length, match.Length);
+        }
+
+        return stringBuilder.ToString();
+    }
+
+    private static string TranslateWord(string word)
+    {
+        if (word.Length == 1)
+        {
+            return word + "ay";
+        }
+
+        return word[1..] + word[0] + "ay";
+    }
+}

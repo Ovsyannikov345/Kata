1c6c705df686a64ce80b6d8780c74d9097e8aab8
Ilya Ovsyannikov <ilya.ovsyannikov345@gmail.com>
2023-08-16 22:39:58 +0300

    feat: add new kata

---
+++ [6kyu]Write Number in Expanded Form/[6kyu]Write Number in Expanded Form/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class Kata
+{
+    public static string ExpandedForm(long number)
+    {
+        List<long> parts = new();
+
+        long i = 10;
+
+        while (number != 0)
+        {
+            long part = number % i;
+
+            if (part != 0)
+            {
+                parts.Add(part);
+                number -= part;
+            }
+
+            i *= 10;
+        }
+
+        parts.Reverse();
+
+        return string.Join(" + ", parts);
+    }
+}

b84a4d0383786b237cd28d6a029ee0f335112336
Ilya Ovsyannikov <ilya.ovsyannikov345@gmail.com>
2023-08-16 23:23:18 +0300

    feat: add new kata

---
+++ [5kyu]First non-repeating character/[5kyu]First non-repeating character/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Kata
+{
+    public static string FirstNonRepeatingLetter(string text)
+    {
+        char[] symbols = text.ToLower().ToCharArray();
+
+        HashSet<char> foundSymbols = new();
+
+        List<char> uniqueSymbols = new();
+
+        foreach (char symbol in symbols)
+        {
+            if (!foundSymbols.Add(symbol))
+            {
+                uniqueSymbols.Remove(symbol);
+            }
+            else
+            {
+                uniqueSymbols.Add(symbol);
+            }
+        }
+
+        if (uniqueSymbols.Count == 0)
+        {
+            return "";
+        }
+
+        if (text.IndexOf(uniqueSymbols[0]) != -1)
+        {
+            return uniqueSymbols[0].ToString();
+        }
+        else
+        {
+            return uniqueSymbols[0].ToString().ToUpper();
+        }
+    }
+}

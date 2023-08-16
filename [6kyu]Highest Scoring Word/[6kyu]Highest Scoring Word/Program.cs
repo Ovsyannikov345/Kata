648da8c06879031d760de28edc9541388119c06b
Ilya Ovsyannikov <ilya.ovsyannikov345@gmail.com>
2023-08-16 22:01:36 +0300

    feat: add new kata

---
+++ [6kyu]Highest Scoring Word/[6kyu]Highest Scoring Word/Program.cs
@@ -0,0 +1,36 @@
+﻿public class Kata
+{
+    public static string High(string source)
+    {
+        string[] words = source.Split();
+
+        int maxScore = GetScore(words[0]);
+
+        int winnerIndex = 0;
+
+        for (var i = 1; i < words.Length; i++)
+        {
+            int score = GetScore(words[i]);
+
+            if (score > maxScore)
+            {
+                maxScore = score;
+                winnerIndex = i;
+            }
+        }
+
+        return words[winnerIndex];
+    }
+
+    private static int GetScore(string word)
+    {
+        int score = 0;
+
+        foreach (char letter in word)
+        {
+            score += letter - 96;
+        }
+
+        return score;
+    }
+}

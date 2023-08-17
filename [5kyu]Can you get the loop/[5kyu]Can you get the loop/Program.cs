23de99f6a08ca2491eb59db0f6fcc9609061da48
Ilya Ovsyannikov <ilya.ovsyannikov345@gmail.com>
2023-08-17 16:12:27 +0300

    feat: add new kata

---
+++ [5kyu]Can you get the loop/[5kyu]Can you get the loop/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class Kata
+{
+    public static int getLoopSize(LoopDetector.Node startNode)
+    {
+        var fastPointer = startNode;
+
+        var slowPointer = startNode;
+
+        do
+        {
+            fastPointer = fastPointer.next.next;
+            slowPointer = slowPointer.next;
+        }
+        while (fastPointer != slowPointer);
+
+        int loopLength = 0;
+
+        do
+        {
+            fastPointer = fastPointer.next;
+            loopLength++;
+        }
+        while (fastPointer != slowPointer);
+
+        return loopLength;
+    }
+}

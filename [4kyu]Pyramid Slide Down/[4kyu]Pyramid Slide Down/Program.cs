a99800854e1c0547880ca452b73ce420b600ba26
Ilya Ovsyannikov <ilya.ovsyannikov345@gmail.com>
2023-08-17 17:05:22 +0300

    feat: add new kata

---
+++ [4kyu]Pyramid Slide Down/[4kyu]Pyramid Slide Down/Program.cs
@@ -0,0 +1,22 @@
+﻿public class PyramidSlideDown
+{
+    public static int LongestSlideDown(int[][] pyramid)
+    {
+        for (var i = pyramid.Length - 2; i >= 0; i--)
+        {
+            for (var j = 0; j < pyramid[i].Length; j++)
+            {
+                if (pyramid[i + 1][j] > pyramid[i + 1][j + 1])
+                {
+                    pyramid[i][j] += pyramid[i + 1][j];
+                }
+                else
+                {
+                    pyramid[i][j] += pyramid[i + 1][j + 1];
+                }
+            }
+        }
+
+        return pyramid[0][0];
+    }
+}
